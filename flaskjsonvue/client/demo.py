6499f7ebe8ff599886f4b23b9330afccb3802605
raphiniert <dev@raphiniert.eu>
2022-04-06 23:28:43 +0200

    fixed url_prefix

--- flaskjsonvue/client/demo.py
+++ flaskjsonvue/client/demo.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template
 
 logger = logging.getLogger("flaskjsonvue")
 
-bp = Blueprint("demo", __name__, url_prefix="/deo")
+bp = Blueprint("demo", __name__, url_prefix="/demo")
 
 
 @bp.route("/", methods=("GET",))
