4d5fc88cacffbaaf7182ce4bf712f6f322314306
raphiniert <dev@raphiniert.eu>
2022-04-18 11:15:22 +0200

    added api v1 demo list endpoint test

---
+++ tests/api/v1/test_demo_api.py
@@ -0,0 +1,14 @@
+def test_demo_api_list(client):
+    """
+    test demo api list
+    :param client:
+    :return:
+    """
+    # get response
+    response = client.get("/api/v1/demo/list")
+    # assert status code
+    assert response.status_code == 200
+    # asser response is json
+    assert response.is_json
+    # assert content is empty list
+    assert response.json == []
