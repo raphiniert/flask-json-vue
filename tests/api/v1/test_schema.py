688335d13846441aecb8da45a3ba2fcb43689e80
raphiniert <dev@raphiniert.eu>
2022-09-14 16:12:47 +0200

    implemented demo json schema tests

--- tests/api/v1/test_schema.py
+++ tests/api/v1/test_schema.py
@@ -2,6 +2,7 @@ import pytest
 import unittest
 
 from flaskjsonvue.api.v1.schemes.base import BaseJsonSchema
+from flaskjsonvue.api.v1.schemes.demo import DemoJsonSchema
 
 #
 # BaseJsonSchema
@@ -175,6 +176,172 @@ class BaseJsonSchemaTest(unittest.TestCase):
     def tearDown(self) -> None:
         """Clean up run after every test method"""
         pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """Method called after tests in an individual class have run"""
+        pass
+
+
+class TestDemoJsonSchema(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Setup test data once for all class methods"""
+        cls.demo_schema = DemoJsonSchema()
+
+    def setUp(self) -> None:
+        """Set up run once for every test method"""
+        pass
+
+    def test_schema_base_url(self):
+        assert self.demo_schema.schema_base_url() == "/api/v1/schema/demo"
+
+    def tearDown(self) -> None:
+        """Clean up run after every test method"""
+        pass
+
+    def test_schema_detail(self):
+        assert self.demo_schema.detail() == {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "$id": "/api/v1/schema/demo/detail.schema.json",
+            "title": "Demo",
+            "description": "Demo JSON object",
+            "type": "object",
+            "relations": {},
+            "properties": {
+                "entry_date": {
+                    "description": f"The demo's entry date.",
+                    "type": "string",
+                    "format": "date-time",
+                },
+                "decimal_number": {
+                    "description": f"The demo's decimal number.",
+                    "type": "number",
+                },
+                "id": {
+                    "description": "The demo's unique identifier",
+                    "type": "integer",
+                    "min": 1,
+                },
+                "name": {
+                    "description": "The demo's name.",
+                    "type": "string",
+                    "minLength": 1,
+                },
+            },
+            "required": ["id", "name", "entry_date", "decimal_number"],
+        }
+
+    def test_schema_create(self):
+        assert self.demo_schema.create() == {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "$id": "/api/v1/schema/demo/create.schema.json",
+            "title": "Demo",
+            "description": "Demo JSON object",
+            "type": "object",
+            "relations": {},
+            "properties": {
+                "entry_date": {
+                    "description": f"The demo's entry date.",
+                    "type": "string",
+                    "format": "date-time",
+                },
+                "decimal_number": {
+                    "description": f"The demo's decimal number.",
+                    "type": "number",
+                },
+                "name": {
+                    "description": "The demo's name.",
+                    "type": "string",
+                    "minLength": 1,
+                },
+            },
+            "required": ["name", "entry_date", "decimal_number"],
+        }
+
+    def test_schema_update(self):
+        assert self.demo_schema.update() == {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "$id": "/api/v1/schema/demo/update.schema.json",
+            "title": "Demo",
+            "description": "Demo JSON object",
+            "type": "object",
+            "relations": {},
+            "properties": {
+                "entry_date": {
+                    "description": f"The demo's entry date.",
+                    "type": "string",
+                    "format": "date-time",
+                },
+                "decimal_number": {
+                    "description": f"The demo's decimal number.",
+                    "type": "number",
+                },
+                "id": {
+                    "description": "The demo's unique identifier",
+                    "type": "integer",
+                    "min": 1,
+                },
+                "name": {
+                    "description": "The demo's name.",
+                    "type": "string",
+                    "minLength": 1,
+                },
+            },
+            "required": ["id", "name", "entry_date", "decimal_number"],
+        }
+
+    def test_schema_delete(self):
+        assert self.demo_schema.delete() == {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "$id": "/api/v1/schema/demo/delete.schema.json",
+            "title": "Demo",
+            "description": "Demo JSON object",
+            "type": "object",
+            "relations": {},
+            "properties": {
+                "id": {
+                    "description": "The demo's unique identifier",
+                    "type": "integer",
+                    "min": 1,
+                },
+            },
+            "required": ["id"],
+        }
+
+    def test_schema_array(self):
+        assert self.demo_schema.array() == {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "$id": "/api/v1/schema/demo/array.schema.json",
+            "title": "Demo",
+            "description": "Array of Demo objects",
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "entry_date": {
+                        "description": f"The demo's entry date.",
+                        "type": "string",
+                        "format": "date-time",
+                    },
+                    "decimal_number": {
+                        "description": f"The demo's decimal number.",
+                        "type": "number",
+                    },
+                    "id": {
+                        "description": "The demo's unique identifier",
+                        "type": "integer",
+                        "min": 1,
+                    },
+                    "name": {
+                        "description": "The demo's name.",
+                        "type": "string",
+                        "minLength": 1,
+                    },
+                },
+                "required": ["id", "name", "entry_date", "decimal_number"],
+            },
+        }
 
     @classmethod
     def tearDownClass(cls):
