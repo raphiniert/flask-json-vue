8814a62ff6a8d403a5548c5588cf9552fc0dfc7f
raphiniert <dev@raphiniert.eu>
2022-09-14 15:17:25 +0200

    added demo json schema

---
+++ flaskjsonvue/api/v1/schemes/demo.py
@@ -0,0 +1,45 @@
+from flaskjsonvue.api.v1.schemes.base import BaseJsonSchema
+
+
+class DemoSchema(BaseJsonSchema):
+    title = "Demo"
+    description = "Demo JSON Object"
+    relations = []
+
+    @classmethod
+    def detail(cls):
+        return {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "$id": f"{cls.schema_base_url()}/detail.schema.json",
+            "title": cls.title,
+            "description": cls.description,
+            "type": "object",
+            "relations": cls.relations,
+            "properties": {
+                "id": {
+                    "description": f"The {cls.title.lower()}'s unique identifier",
+                    "type": "integer",
+                    "min": 1,
+                },
+                "name": {
+                    "type": ["string", "null"],
+                    "description": f"The {cls.title.lower()}' name.",
+                    "minLength": 1,
+                },
+                "entry_date": {
+                    "description": f"The {cls.title.lower()}'s entry date.",
+                    "type": "string",
+                    "format": "date-time",
+                },
+                "decimal_number": {
+                    "description": f"The {cls.title.lower()}'s decimal number.",
+                    "type": "number",
+                },
+            },
+            "required": [
+                "id",
+                "name",
+                "entry_date",
+                "decimal_number",
+            ],
+        }
