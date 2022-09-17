f95b8a4de935533c6bdc9ab843f8741508037949
raphiniert <dev@raphiniert.eu>
2022-09-17 12:16:02 +0200

    render detail template for demo client /add route

--- flaskjsonvue/client/demo.py
+++ flaskjsonvue/client/demo.py
@@ -10,3 +10,8 @@ bp = Blueprint("demo", __name__, url_prefix="/demo")
 @bp.route("/", methods=("GET",))
 def index():
     return render_template(f"list.html", obj_type=bp.name)
+
+
+@bp.route("/add", methods=("GET",))
+def create():
+    return render_template(f"detail.html", obj_type=bp.name)
--- flaskjsonvue/static/vue/components/object/List.vue
+++ flaskjsonvue/static/vue/components/object/List.vue
@@ -22,9 +22,9 @@ async function getObjectList() {
   }
 }
 
-const add_url = computed(() => {
-  return `api/v1/${props.objType}/add`;
-})
+// const add_url = computed(() => {
+//   return `${props.objType}/add`;
+// })
 
 // lifecycle hooks
 onMounted(() => {
@@ -37,7 +37,7 @@ onMounted(() => {
 <template>
   <h1>{{ props.objType }} list component</h1>
   <template v-if="loadedObjects">
-    <a :href="add_url" class="btn btn-outline-primary">
+    <a href="add" class="btn btn-outline-primary">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
