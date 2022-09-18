f5b2c849f812de24ef9ee95c88a6737fed3ed506
raphiniert <dev@raphiniert.eu>
2022-09-18 21:21:58 +0200

    use absolute url path for add link in list view

--- flaskjsonvue/static/vue/components/object/List.vue
+++ flaskjsonvue/static/vue/components/object/List.vue
@@ -23,13 +23,13 @@ async function getObjectList() {
 }
 
 // const add_url = computed(() => {
-//   return `${props.objType}/add`;
+//   return `/${props.objType}/add`;
 // })
 
 // lifecycle hooks
 onMounted(() => {
   console.log(`Initial log message.`, loadedObjects.value);
-  getObjectList(props.objType);
+  getObjectList();
   console.log(objectList.value, loadedObjects.value);
 })
 </script>
@@ -37,7 +37,7 @@ onMounted(() => {
 <template>
   <h1>{{ props.objType }} list component</h1>
   <template v-if="loadedObjects">
-    <a href="add" class="btn btn-outline-primary">
+    <a :href="`/${props.objType}/add`" class="btn btn-outline-primary">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
