39af3e4f96ef268ecfd0742c3abc87767ac83aa5
raphiniert <dev@raphiniert.eu>
2022-09-18 21:28:40 +0200

    load detail component in detail.html, added tests

--- tests/client/test_demo_client.py
+++ tests/client/test_demo_client.py
@@ -10,3 +10,17 @@ def test_demo_client_index(client):
     assert response.status_code == 200
     # assert content
     assert b'<list-component obj-type="demo"></list-component>' in response.data
+
+
+def test_demo_client_create(client):
+    """
+    test demo client index
+    :param client:
+    :return:
+    """
+    # get response
+    response = client.get("/demo/add")
+    # assert status code
+    assert response.status_code == 200
+    # assert content
+    assert b'<detail-component obj-type="demo"></detail-component>' in response.data
