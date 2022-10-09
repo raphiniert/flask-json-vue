d78424dcef2eb2e4c902c2c2ebab2508d4d3b280
raphiniert <dev@raphiniert.eu>
2022-10-09 12:19:27 +0200

    list obj display names

--- flaskjsonvue/static/vue/components/object/List.vue
+++ flaskjsonvue/static/vue/components/object/List.vue
@@ -37,6 +37,13 @@ onMounted(() => {
 <template>
   <h1>{{ props.objType }} list component</h1>
   <template v-if="loadedObjects">
+    <div :class="`list-container ${props.objType}`">
+      <div v-for="(obj, index) in objectList"  :key="index" class="row">
+        <div class="col">
+          {{ obj.meta.display_name }}
+        </div>
+      </div>
+    </div>
     <a :href="`/${props.objType}/add`" class="btn btn-outline-primary">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
