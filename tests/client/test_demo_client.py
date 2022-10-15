e4f18c0cb6e015d525d4835f1b9e05250547b55f
raphiniert <dev@raphiniert.eu>
2022-10-15 10:11:35 +0200

    added client demo update test

--- tests/client/test_demo_client.py
+++ tests/client/test_demo_client.py
@@ -24,3 +24,20 @@ def test_demo_client_create(client):
     assert response.status_code == 200
     # assert content
     assert b'<detail-component obj-type="demo"></detail-component>' in response.data
+
+
+def test_demo_client_update(client):
+    """
+    test demo client index
+    :param client:
+    :return:
+    """
+    # get response
+    response = client.get(f"/demo/update/{1}")
+    # assert status code
+    assert response.status_code == 200
+    # assert content
+    assert (
+        b'<detail-component obj-type="demo" :obj-id="1"></detail-component>'
+        in response.data
+    )
