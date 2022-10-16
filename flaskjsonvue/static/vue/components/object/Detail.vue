49deb0b52b44ff61716ae66c088b71879bbf4598
raphiniert <dev@raphiniert.eu>
2022-10-16 18:36:34 +0200

    added alert messages

--- flaskjsonvue/static/vue/components/object/Detail.vue
+++ flaskjsonvue/static/vue/components/object/Detail.vue
@@ -12,6 +12,9 @@
   // reactive state
   const obj = ref({});
   const errors = ref([])
+  const warnings = ref([])
+  const success = ref([])
+  const infos = ref([])
   const schema = ref({});
   const loadedObject = ref(false);
   const loadedSchema = ref(false);
@@ -56,6 +59,10 @@
     if(gResponse.status == 201) {
       // show success message
       obj.value = response.objects[0].data;
+      errors.value = response.errors
+      warnings.value = response.warnings
+      infos.value = response.infos
+      success.value = response.success
       window.location = `http://0.0.0.0:5000/${props.objType}/update/${obj.value.id}`
     } else {
       errors.value = response.errors
@@ -72,7 +79,12 @@
     if(gResponse.status == 200) {
       obj.value = response.objects[0].data;
       loadedObject.value = true;
+      errors.value = response.errors
+      warnings.value = response.warnings
+      success.value = response.success
+      infos.value = response.infos
     } else {
+      errors.value = response.errors
       console.error(`Couldn't get ${props.objType} with id: ${props.objId}!`)
     }
   }
@@ -92,7 +104,10 @@
     if(gResponse.status == 200) {
       // show success message
       obj.value = response.objects[0].data;
-      errors.value = []
+      errors.value = response.errors
+      warnings.value = response.warnings
+      success.value = response.success
+      infos.value = response.infos
     } else {
       errors.value = response.errors;
       console.error(`Couldn't update ${props.objType} with id: ${props.objId}!`)
@@ -112,8 +127,13 @@
     });
     const response = await gResponse.json();
     if(gResponse.status == 200) {
+      errors.value = response.errors
+      warnings.value = response.warnings
+      success.value = response.success
+      infos.value = response.infos
       window.location = `http://0.0.0.0:5000/${props.objType}`;
     } else {
+      errors.value = response.errors
       console.error(`Couldn't delete ${props.objType} with id: ${props.objId}!`)
     }
   }
@@ -198,6 +218,50 @@
       <div class="col">
         {{ schema.description }}
       </div>
+    </div>
+    <div v-if="errors.length" class="alert alert-danger alert-dismissible fade show" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+      </svg>
+      <ul class="mb-0 d-inline-block">
+        <li v-for="error in errors">
+          <a :href="`#${props.objType}-prop-${error.field}`" class="alert-link">{{ error.field }}</a>: {{ error.message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div v-if="warnings.length" class="alert alert-warning alert-dismissible fade show" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+      </svg>
+      <ul class="mb-0 d-inline-block">
+        <li v-for="message in warnings">
+          {{ message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div v-if="success.length" class="alert alert-success alert-dismissible fade show" role="alert">
+      <svg class="pt-0" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+      </svg>
+      <ul class="mb-0 d-inline-block">
+        <li v-for="message in success">
+          {{ message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div v-if="infos.length" class="alert alert-info alert-dismissible fade show" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+      </svg>
+      <ul class="mb-0  d-inline-block">
+        <li v-for="message in infos">
+          {{ message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     <!-- do not display id property -->
     <form @submit.prevent="formSubmit()" class="needs-validation" novalidate>
--- flaskjsonvue/static/vue/components/object/List.vue
+++ flaskjsonvue/static/vue/components/object/List.vue
@@ -9,6 +9,10 @@ const props = defineProps({
 // reactive state
 const objectList = ref([]);
 const loadedObjects = ref(false);
+const errors = ref([])
+const warnings = ref([])
+const success = ref([])
+const infos = ref([])
 
 // functions that mutate state and trigger updates
 async function getObjectList() {
@@ -33,7 +37,12 @@ async function deleteObject(obj) {
   const response = await gResponse.json();
   if(gResponse.status == 200) {
     getObjectList()
+    errors.value = response.errors
+    warnings.value = response.warnings
+    success.value = response.success
+    infos.value = response.infos
   } else {
+
     console.error(`Couldn't delete ${props.objType} with id: ${obj.data.id}!`)
   }
 }
@@ -49,6 +58,50 @@ onMounted(() => {
 <template>
   <h1>{{ props.objType }} list component</h1>
   <template v-if="loadedObjects">
+    <div v-if="errors.length" class="alert alert-danger alert-dismissible fade show" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+      </svg>
+      <ul class="mb-0 d-inline-block">
+        <li v-for="error in errors">
+          <a :href="`#${props.objType}-prop-${error.field}`" class="alert-link">{{ error.field }}</a>: {{ error.message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div v-if="warnings.length" class="alert alert-warning alert-dismissible fade show" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+      </svg>
+      <ul class="mb-0 d-inline-block">
+        <li v-for="message in warnings">
+          {{ message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div v-if="success.length" class="alert alert-success alert-dismissible fade show" role="alert">
+      <svg class="pt-0" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+      </svg>
+      <ul class="mb-0 d-inline-block">
+        <li v-for="message in success">
+          {{ message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div v-if="infos.length" class="alert alert-info alert-dismissible fade show" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+      </svg>
+      <ul class="mb-0  d-inline-block">
+        <li v-for="message in infos">
+          {{ message }}
+        </li>
+      </ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
     <div :class="`list-container ${props.objType}`">
       <div v-for="(obj, index) in objectList"  :key="index" class="row">
         <div class="col">
