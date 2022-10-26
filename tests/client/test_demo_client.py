96e294ec1ca431b880ad93eff234222140a90c99
raphiniert <dev@raphiniert.eu>
2022-10-26 23:05:46 +0200

    removed custom component tests, check if router-view is present

--- tests/client/test_demo_client.py
+++ tests/client/test_demo_client.py
@@ -10,34 +10,3 @@ def test_demo_client_index(client):
     assert response.status_code == 200
     # assert content
     assert b'<list-component obj-type="demo"></list-component>' in response.data
-
-
-def test_demo_client_create(client):
-    """
-    test demo client index
-    :param client:
-    :return:
-    """
-    # get response
-    response = client.get("/demo/add")
-    # assert status code
-    assert response.status_code == 200
-    # assert content
-    assert b'<detail-component obj-type="demo"></detail-component>' in response.data
-
-
-def test_demo_client_update(client):
-    """
-    test demo client index
-    :param client:
-    :return:
-    """
-    # get response
-    response = client.get(f"/demo/update/{1}")
-    # assert status code
-    assert response.status_code == 200
-    # assert content
-    assert (
-        b'<detail-component obj-type="demo" :obj-id="1"></detail-component>'
-        in response.data
-    )
