c6250b561afff8c05ea9b490369ea8afa192a222
raphiniert <dev@raphiniert.eu>
2022-10-26 23:26:11 +0200

    render base.html only, removed add and update routes

--- flaskjsonvue/client/demo.py
+++ flaskjsonvue/client/demo.py
@@ -9,14 +9,4 @@ bp = Blueprint("demo", __name__, url_prefix="/demo")
 
 @bp.route("/", methods=("GET",))
 def index():
-    return render_template(f"list.html", obj_type=bp.name)
-
-
-@bp.route("/add", methods=("GET",))
-def create():
-    return render_template(f"detail.html", obj_type=bp.name)
-
-
-@bp.route("/update/<int:obj_id>", methods=("GET",))
-def update(obj_id):
-    return render_template(f"detail.html", obj_type=bp.name, obj_id=obj_id)
+    return render_template(f"base.html")
