1a39a6784a5aa683cecd6c2e8da26c926bb1630f
raphiniert <dev@raphiniert.eu>
2022-11-06 21:29:32 +0100

    added Home and NotFound view, use in router

--- flaskjsonvue/static/vue/router/index.js
+++ flaskjsonvue/static/vue/router/index.js
@@ -1,29 +1,22 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
 import ObjectList from '../components/object/List.vue';
 import ObjectDetail from '../components/object/Detail.vue';
+import Home from '../views/Home.vue';
+import NotFound from '../views/NotFound.vue';
 
-// 1. Define route components.
-// These can be imported from other files
-const Home = { template: '<div>Home</div>' }
-
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 // TODO: call endpoint with objtypes,
-
 const routes = [
   // will match everything and put it under `$route.params.pathMatch`
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
   { name: 'root', path: '/', component: Home },
   { name: 'list', path: '/:objtype(demo|address)', component: ObjectList },
   { name: 'add', path: '/:objtype(demo|address)/add', component: ObjectDetail },
   { name: 'update', path: '/:objtype(demo|address)/update/:id', component: ObjectDetail },
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
+// create the router instance and pass the `routes` option
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  // Provide the history implementation to use.
   history: createWebHashHistory(),
   routes, // short for `routes: routes`,
   scrollBehavior(to, from, savedPosition) {
---
+++ flaskjsonvue/static/vue/views/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>404</h1>
+    <p>Page <mark>{{ $route.path }}</mark> was not found... <router-link :to="{ name: 'root' }">Home</router-link></p>
+</template>
